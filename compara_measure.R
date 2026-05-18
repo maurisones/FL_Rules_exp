711a358e953f06f9ce3501b0e320ca1839c61f16
Mauri Ferrandin <maurisones@gmail.com>
2026-05-18 10:20:14 -0300

    Apenas um update do R script esquecido no push anterior

--- compara_measure.R
+++ compara_measure.R
@@ -11,6 +11,105 @@ measures = c("measures-Accuracy-all", "measures-FMeasureWeightedAvg-all", "measu
 algs <- c("RuleMatchCount.PART", "RuleMatchWeighted.PART", "RuleMatchCount.J48",   "RuleMatchWeighted.J48", "RuleMatchCount.DT",     
            "RuleMatchCount.Rand",    "RuleMatchWeighted.Rand")
 
+
+generate_ranking <- function(data){
+  
+  retorno = list()
+  coluna = ncol(data)
+  linha = nrow(data)
+  
+  rank_first_0 = data.frame()
+  rank_last_0 = data.frame()
+  rank_average_0 = data.frame()
+  rank_random_0 = data.frame()
+  rank_min_0 = data.frame()
+  rank_max_0 = data.frame()
+  
+  i = 1
+  for(i in 1:linha){
+    rf = rank(data[i,], ties.method = "first")     # first occurrence wins
+    rl = rank(data[i,], ties.method = "last")      # last occurrence wins
+    rav = rank(data[i,], ties.method = "average")   # média
+    ran = rank(data[i,], ties.method = "random")    # ordem aleatória
+    rma = rank(data[i,], ties.method = "max")       # máximo
+    rmi = rank(data[i,], ties.method = "min")       # mínimo
+    
+    rank_first_0 = rbind(rank_first_0, rf)
+    rank_last_0 = rbind(rank_last_0, rl)
+    rank_average_0 = rbind(rank_average_0, rav)
+    rank_random_0 = rbind(rank_random_0, ran)
+    rank_max_0 = rbind(rank_max_0, rma)
+    rank_min_0 = rbind(rank_min_0, rmi)
+    
+  }
+  
+  colnames(rank_first_0) = colnames(data)
+  colnames(rank_last_0) = colnames(data)
+  colnames(rank_average_0) = colnames(data)
+  colnames(rank_random_0) = colnames(data)
+  colnames(rank_max_0) = colnames(data)
+  colnames(rank_min_0) = colnames(data)
+  
+  
+  rank_first_1 = data.frame()
+  rank_last_1 = data.frame()
+  rank_average_1 = data.frame()
+  rank_random_1 = data.frame()
+  rank_min_1 = data.frame()
+  rank_max_1 = data.frame()
+  
+  i = 1
+  for(i in 1:linha){
+    rf = (coluna -  rank_first_0[i,]) +1
+    rl = (coluna - rank_last_0[i,]) +  1
+    rav = (coluna - rank_average_0[i,]) + 1
+    ran = (coluna - rank_random_0[i,]) + 1
+    rma = (coluna - rank_max_0[i,]) + 1
+    rmi = (coluna - rank_min_0[i,]) + 1
+    
+    rank_first_1 = rbind(rank_first_1, rf)
+    rank_last_1 = rbind(rank_last_1, rl)
+    rank_average_1 = rbind(rank_average_1, rav)
+    rank_random_1 = rbind(rank_random_1, ran)
+    rank_max_1 = rbind(rank_max_1, rma)
+    rank_min_1 = rbind(rank_min_1, rmi)
+  }
+  
+  colnames(rank_first_1) = colnames(data)
+  colnames(rank_last_1) = colnames(data)
+  colnames(rank_average_1) = colnames(data)
+  colnames(rank_random_1) = colnames(data)
+  colnames(rank_max_1) = colnames(data)
+  colnames(rank_min_1) = colnames(data)
+  
+  rank_average_0 = trunc(rank_average_0,0)
+  rank_average_1 = trunc(rank_average_1,0)
+  
+  retorno$rank_first_0 = rank_first_0
+  retorno$rank_last_0 = rank_last_0
+  retorno$rank_average_0 = rank_average_0
+  retorno$rank_random_0 = rank_random_0
+  retorno$rank_max_0 = rank_max_0
+  retorno$rank_min_0 = rank_min_0
+  
+  retorno$rank_first_1 = rank_first_1
+  retorno$rank_last_1 = rank_last_1
+  retorno$rank_average_1 = rank_average_1
+  retorno$rank_random_1 = rank_random_1
+  retorno$rank_max_1 = rank_max_1
+  retorno$rank_min_1 = rank_min_1
+  
+  return(retorno)
+}
+
+
+
+
+
+
+
+
+
 for (m in measures){
   acc3 <- read.csv(paste(output_dir, "results-3/", m,".csv", sep = ""), row.names = 1)
   acc5 <- read.csv(paste(output_dir, "results-5/", m,".csv", sep = ""), row.names = 1)
@@ -39,6 +138,20 @@ for (m in measures){
     postscript(paste(output_dir, m, "-", c,"-evolution.eps", sep=""))
     plotCD(dfc, alpha=0.05, cex=1.3)
     dev.off()
+    
+    
+    
+    # add average
+    dfc <- rbind(dfc, colMeans(dfc))
+    rownames(dfc)[nrow(dfc)] <- "Average"
+    
+    # add rank and media aos resultados
+    ranking <- generate_ranking(dfc)
+    ranking <- ranking$rank_average_1
+    dfc <- rbind(dfc, colSums(ranking))
+    rownames(dfc)[nrow(dfc)] <- "RankSum"
+    
+    write.csv(dfc, paste(output_dir, m, "-", c,"-evolution.csv", sep=""))
     
   }
 }
